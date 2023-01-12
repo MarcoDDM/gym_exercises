6c773802ea7b6a987219dfbfb87c3b4e0e0f89a8
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-01-12 16:11:54 -0300

    Components and Pages folders

--- gym/src/App.js
+++ gym/src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+
 
 const App = () => {
   return (
